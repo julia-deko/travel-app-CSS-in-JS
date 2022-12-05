a57f42b936f39b5c8adf0f2fee183ef04ed84b11
Julia <julia.dzianisenka@gmail.com>
2022-12-05 14:41:31 +0100

    add styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+/** @jsxImportSource @emotion/react */
 import logo from "./logo.png";
+import { css, ThemeProvider } from "@emotion/react";
+import {
+  CardWrapper,
+  ImageWrapper,
+  TextWrapper,
+  TitleWrapper,
+  DescriptionWrapper,
+  ActionsWrapper,
+  PrimaryButton,
+  SecondaryButton,
+  theme,
+  LogoSpin
+} from "./styles";
+
 // import `css`and `ThemeProvider` from "@emotion/react" package
 // import styled components, theming and animation from "./styles.js" file
 
@@ -28,28 +43,57 @@ const hotels = [
 
 // Apply styling to code within the `App` component's `return` statement using styled components, theming, animation and the `css` prop
 function App() {
-  return (
-    <main>
-      <img src={logo} alt="" />
-      <div>
+  return <ThemeProvider theme={theme}>
+      <main
+       css={(theme) => ({
+        color: theme.colors.primary,
+        background: theme.colors.secondary,
+        height: "1200px",
+        fontFamily: theme.fonts.primary,
+      })}
+    >
+      <img
+        src={logo}
+        alt="logo"
+        css={css`
+          display: absolute;
+          margin-top: 15px;
+          margin-left: 15px;
+          height: 100px;
+          width: 100px;
+          animation: ${LogoSpin} 10s linear infinite;
+        `}
+      />
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 15px;
+          padding: 20px;
+          @media (max-width: 900px) {
+            display: grid;
+          }
+        `}
+      >
         {hotels.map((hotel) => {
-          return (
-            <div key={hotel.id}>
-              <img src={hotel.src} alt={hotel.alt} />
-              <div>
-                <h2>{hotel.title}</h2>
-                <h3>{hotel.description}</h3>
-              </div>
-              <div>
-                <button>Details</button>
-                <button>Book</button>
-              </div>
-            </div>
-          );
-        })}
+      return (
+          <CardWrapper key={hotel.id}>
+              <ImageWrapper src={hotel.src} alt={hotel.alt} />
+              <TextWrapper>
+                  <TitleWrapper>{hotel.title}</TitleWrapper>
+                  <DescriptionWrapper>{hotel.description}</DescriptionWrapper>
+              </TextWrapper>
+              <ActionsWrapper>
+                    <PrimaryButton>Details</PrimaryButton>
+                    <SecondaryButton>Book</SecondaryButton>
+              </ActionsWrapper>
+          </CardWrapper>
+      );
+    })}
       </div>
     </main>
-  );
+  </ThemeProvider>
 }
 
 export default App;
